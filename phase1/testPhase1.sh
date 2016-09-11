f632796bcb1d439458d5d671419b37bf12db7fb3
Austin George <jaustingeorge@gmail.com>
2016-09-11 00:59:33 -0700

    Script for testing Phase1

---
+++ phase1/testPhase1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for i in $(seq $1 $2)
+do
+    if [ $i -gt 9 ]
+    then
+        number=$i
+    else
+        number="0$i"
+    fi
+
+    echo "----------- test0$number -----------"
+    make $(echo "test$number") 
+    echo
+    ./$(echo "test$number") &> diffTests/$(echo "test$number""diff")
+    echo
+    diff $(echo "diffTests/test$number""diff") $(echo "testResults/test$number"".txt")
+    echo
+done
